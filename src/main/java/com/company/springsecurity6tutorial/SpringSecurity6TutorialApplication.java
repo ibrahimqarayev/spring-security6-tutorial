47f8190fd38c29f9652726c847d7a5561131d716
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-14 16:16:36 +0400

    Initial commit

---
+++ src/main/java/com/company/springsecurity6tutorial/SpringSecurity6TutorialApplication.java
@@ -0,0 +1,13 @@
+package com.company.springsecurity6tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6TutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurity6TutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/springsecurity6tutorial/SpringSecurity6TutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.springsecurity6tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6TutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
