2f84f9beeb6ae3f4a732b9d0c1ab7b122fd3836b
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-15 22:28:49 +0400

    Created SecurityConfiguration

---
+++ src/main/java/com/company/springsecurity6tutorial/config/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package com.company.springsecurity6tutorial.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+}
