f22dfc34a88d3417d808b380d8b1e7a79df64839
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-04-21 23:55:09 +0400

    Added JwtService

--- src/main/java/com/company/springsecurity6tutorial/service/JwtService.java
+++ src/main/java/com/company/springsecurity6tutorial/service/JwtService.java
@@ -1,11 +1,23 @@
 package com.company.springsecurity6tutorial.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
 
-    public String extractUsername(String jwt) {
+    public String extractUsername(String token) {
         return null;
     }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
 }
