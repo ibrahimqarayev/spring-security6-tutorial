1926e0bca6a9f30d4c85f6afeea7ab80355e5dbf
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-01 23:19:30 +0400

    Fixed AuthenticationController

--- src/main/java/com/company/springsecurity6tutorial/auth/AuthenticationController.java
+++ src/main/java/com/company/springsecurity6tutorial/auth/AuthenticationController.java
@@ -4,16 +4,12 @@ import com.company.springsecurity6tutorial.request.LoginRequest;
 import com.company.springsecurity6tutorial.request.RegisterRequest;
 import com.company.springsecurity6tutorial.response.AuthenticationResponse;
 import com.company.springsecurity6tutorial.service.AuthenticationService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -30,13 +26,8 @@ public class AuthenticationController {
     }
 
     @PostMapping("/authenticate")
-    public ResponseEntity<AuthenticationResponse> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(service.login(request));
-    }
-
-    @PostMapping("/refresh-token")
-    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        service.refreshToken(request, response);
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(service.authenticate(request));
     }
 
 
